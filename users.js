c89a3c194b7f231e58a44b67543c5a90231976f1
ayushpanda862 <pandayush2009@gmail.com>
2024-10-12 18:56:06 -0400

    Update users.js

--- users.js
+++ users.js
@@ -1,23 +1,22 @@
-// users.js
 const users = [
     {
-        username: "testaccount1@gmail.com",
-        full_name: "Test Account 1",
-        password: "test1password",
-        last_voted: "2023-11-05"
+        "username": "testaccount1@gmail.com",
+        "full_name": "Test Account 1",
+        "password": "test1password",
+        "last_voted": "2023-11-05"
     },
     {
-        username: "testaccount2@gmail.com",
-        full_name: "Test Account 2",
-        password: "test2password",
-        last_voted: "2024-03-15"
+        "username": "testaccount2@gmail.com",
+        "full_name": "Test Account 2",
+        "password": "test2password",
+        "last_voted": "2024-03-15"
     },
     {
-        username: "user3@gmail.com",
-        full_name: "User Three",
-        password: "user3password",
-        last_voted: "2024-01-20"
+        "username": "user3@gmail.com",
+        "full_name": "User Three",
+        "password": "user3password",
+        "last_voted": "2024-01-20"
     }
 ];
 
-export default users;
+// Do not export the variable
