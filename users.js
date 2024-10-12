977dac945ea9ea80f55904116b52d645f448d2a4
ayushpanda862 <pandayush2009@gmail.com>
2024-10-12 18:51:11 -0400

    Add files via upload

---
+++ users.js
@@ -0,0 +1,23 @@
+// users.js
+const users = [
+    {
+        username: "testaccount1@gmail.com",
+        full_name: "Test Account 1",
+        password: "test1password",
+        last_voted: "2023-11-05"
+    },
+    {
+        username: "testaccount2@gmail.com",
+        full_name: "Test Account 2",
+        password: "test2password",
+        last_voted: "2024-03-15"
+    },
+    {
+        username: "user3@gmail.com",
+        full_name: "User Three",
+        password: "user3password",
+        last_voted: "2024-01-20"
+    }
+];
+
+export default users;
